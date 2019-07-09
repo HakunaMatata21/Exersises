3f052d691b7a2318008de2057b09817dabae9567
HakunaMatata21 <mechevk@gmail.com>
2019-07-09 13:12:13 +0300

    Exersise 1.11

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string years;
+
+            years = Console.ReadLine(); // input
+            int yearsint = int.Parse(years); // string -> integer
+
+            DateTime today = DateTime.Now; // today = today date
+            DateTime future = DateTime.Today.AddYears(10); // future = today date + 10
+            DateTime futureYear = future.AddYears(yearsint); // futureYear = future + age
+            int predict = futureYear.Year - today.Year; // futureYear - today = age + 10
+            Console.WriteLine("Your age 10 years from now will be: " + predict);
+        }
+
+    }
+}
