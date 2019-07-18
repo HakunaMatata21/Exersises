f08b112c5fdae2cd294b5623aede868291ad11fc
HakunaMatata21 <mechevk@gmail.com>
2019-07-18 11:08:54 +0300

    added ex2 and cleaned up the code a bit

--- Program.cs
+++ Program.cs
@@ -3,10 +3,18 @@ namespace app1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            CheckYears();
+            Console.WriteLine("");
+            CheckEven();
+        }
+
+        static void CheckYears()
         {
             string years;
 
+            Console.Write("Input your age: ");
             years = Console.ReadLine(); // input
             int yearsint = int.Parse(years); // string -> integer
 
@@ -15,7 +23,16 @@ namespace app1
             DateTime futureYear = future.AddYears(yearsint); // futureYear = future + age
             int predict = futureYear.Year - today.Year; // futureYear - today = age + 10
             Console.WriteLine("Your age 10 years from now will be: " + predict);
+            
         }
-
+         static void CheckEven()
+        {
+            string number;
+            int x;
+            Console.Write("Input a number to check if it is even: ");
+            number = Console.ReadLine();
+            x = int.Parse(number) % 2;
+            Console.WriteLine(x == 0 ? "The number is even" : "The number is not even");
+        }
     }
 }
